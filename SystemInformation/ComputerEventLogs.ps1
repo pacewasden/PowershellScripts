9e4614766a40c7a7f94cfbe64e65966e69ca5ec3
Pace Wasden <pacewasden@gmail.com>
2024-02-04 08:54:38 -0700

    grabbing last update

--- SystemInformation/ComputerEventLogs.ps1
+++ SystemInformation/ComputerEventLogs.ps1
@@ -1,7 +1,7 @@
 # Get events from the Application event log using Get-WinEvent
 #You can change System for "Security" or "Application" in order to get different 
 #events
-$applicationEvents = Get-WinEvent -LogName System -MaxEvents 10
+$applicationEvents = Get-WinEvent -LogName Application -MaxEvents 100
 
 # Display event information
 $applicationEvents | Format-Table -AutoSize
